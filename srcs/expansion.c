24a9b4623dd1441926b8294db7690512fd191b8a
Valentin LUGAND LOPEZ <vlugand-@e2r3p16.42.fr>
2021-04-09 18:24:50 +0200

    working on env var

--- srcs/expansion.c
+++ srcs/expansion.c
@@ -6,13 +6,13 @@
 /*   By: vlugand- <vlugand-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 11:27:23 by vlugand-          #+#    #+#             */
-/*   Updated: 2021/04/09 15:54:24 by vlugand-         ###   ########.fr       */
+/*   Updated: 2021/04/09 18:24:16 by vlugand-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int		find_env_var(char *s)
+int			is_env_var(char *s)
 {
 	int		i;
 
@@ -28,11 +28,55 @@ int		find_env_var(char *s)
 
 // check aussi file dans rdir
 
-void		env_var_expansion(t_list *exec_lst, t_list *env_var)
+t_list		*var_tokenizer(char *var)
 {
+	char	**split;
+	int		i;
+	t_list	*lst;
+
+	if (var[0] == '\'' || var[0] == '\"')
+		return (ft_lstnew(ft_strdup(var)));
+	split = ft_split(var, ' ');
+	while (split[i])
+	{
+		lst = ft_lstnew(split[i]);
+		lst = lst->next;
+		i++;
+	}
+	free(split);
+	return (lst);
+}
+
+void		replace_token(t_list **exec_lst, t_list *env)
+{
+	int		len;
+	char	*var;
+
+	len = ft_strlen(((char *)((*exec_lst)->content))) - 1;//we don't count the $
+	while (env)
+	{
+		if ((var = ft_strnstr(((char *)(env->content) + 1), ((char *)((*exec_lst)->content)), len)))
+		{
+			if (var[len] == '=')
+				(*exec_lst)->content = ;
+		}
+
+	}
+}
+
+void		env_var_expansion(t_list *exec_lst, t_list *rdir_lst, t_list *env)
+{
+	char	*needle;
+	
 	while (exec_lst)
 	{
-		if (find_env_var((char *)(exec_lst->content))
+		if (is_env_var((char *)(exec_lst->content))
+		{
+			
+		}
+	}
+	while (rdir_lst)
+	{
 		
 	}
 }
