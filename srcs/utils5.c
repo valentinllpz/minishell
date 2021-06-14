33ad45006bbefb5ecdb30b7ce4565e63d841862a
Ade-garr <ade-garr@student.42.fr>
2021-06-14 16:21:08 +0200

    maj shlvl

--- srcs/utils5.c
+++ srcs/utils5.c
@@ -6,7 +6,7 @@
 /*   By: ade-garr <ade-garr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 19:15:49 by ade-garr          #+#    #+#             */
-/*   Updated: 2021/06/14 14:25:22 by ade-garr         ###   ########.fr       */
+/*   Updated: 2021/06/14 16:20:36 by ade-garr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,18 @@ void	add_shlvl(t_shell *shell)
 	char	*s;
 	int		i;
 
-	if env_exists(shell->env, "SHLVL", 5)
+	if (env_exists(shell->env, "SHLVL", 5) == 1)
 	{
 		s = get_value_from_env(shell->env, "SHLVL", 5);
 		if (s != NULL)
 		{
 			i = ft_atoi(s);
-			
+			i = i + 1;
+			s = ft_itoa(i);
+			if (s == NULL)
+				ft_error(shell);
+			change_value_from_env(shell->env, s, "SHLVL", 5);
+			free(s);
 		}
 	}
 }
