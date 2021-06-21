7a836b74bd96d4a0759890ab1fe773247606775b
Valentin LUGAND LOPEZ <vlugand-@e2r10p20.42.fr>
2021-06-21 14:56:46 +0200

    fixed bs

--- srcs/expansion2.c
+++ srcs/expansion2.c
@@ -6,13 +6,13 @@
 /*   By: vlugand- <vlugand-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 15:57:58 by ade-garr          #+#    #+#             */
-/*   Updated: 2021/06/17 15:44:23 by vlugand-         ###   ########.fr       */
+/*   Updated: 2021/06/21 14:55:07 by vlugand-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char	*exp_outliers(char *s, int *len, int return_value)
+char	*dollar_question_mark_exp(char *s, int *len, int return_value)
 {
 	if (s)
 	{
@@ -23,6 +23,29 @@ char	*exp_outliers(char *s, int *len, int return_value)
 		}
 	}
 	return (NULL);
+}
+
+char	*preserve_literal_value(char *match)
+{
+	int		i;
+	int		j;
+	char	*dst;
+
+	dst = malloc(sizeof(char) * (ft_strlen(match) + 3));
+	if (!dst)
+		return (NULL);
+	i = 0;
+	j = 1;
+	dst[0] = '\'';
+	while (match[i])
+	{
+		dst[j] = match[i];
+		i++;
+		j++;
+	}
+	dst[j] = '\'';
+	dst[j + 1] = '\0';
+	return (dst);
 }
 
 char	*find_match_in_env(char *s, int *len, t_list *env)
@@ -37,7 +60,7 @@ char	*find_match_in_env(char *s, int *len, t_list *env)
 	{
 		if (*len > 0 && ft_strncmp(s, (char *)(env->content), *len) == 0
 		&& ((char *)(env->content))[*len] == '=')
-			return ((char *)(env->content) + *len + 1);
+			return (preserve_literal_value((char *)(env->content) + *len + 1));
 		env = env->next;
 	}
 	return (NULL);
@@ -49,13 +72,16 @@ char	*replace_var(char *s, int i, t_list *env, int return_value)
 	char	*outlier;
 	char	*dst;
 	int		len;
-
+	
 	len = 0;
 	match = find_match_in_env(s + i + 1, &len, env);
-	outlier = exp_outliers(s + i + 1, &len, return_value);
+	outlier = dollar_question_mark_exp(s + i + 1, &len, return_value);
 	s[i] = '\0';
 	if (match)
+	{
 		dst = join_three_str(s, match, s + i + len + 1);
+		free(match);
+	}
 	else if (outlier)
 	{
 		dst = join_three_str(s, outlier, s + i + len + 1);
